e550a42ea2e69d9fea7b3fd1b10f6d0daa978047
mslun <mlmosquerap@unal.edu.co>
2017-02-27 16:13:18 -0500

    Descarga canvas
    falta redes sociales

--- GeneradorMemesPaginaRobledo_2/public_html/js/index.js
+++ GeneradorMemesPaginaRobledo_2/public_html/js/index.js
@@ -1,41 +1,42 @@
 
-  var memeSize = 300;
+var memeSize = 300;
 
-  var canvas = document.getElementById('memecanvas');
-  ctx = canvas.getContext('2d');
-
-
-  // Set the text style to that to which we are accustomed
+var canvas = document.getElementById('memecanvas');
+ctx = canvas.getContext('2d');
 
 
 
-  canvas.width = memeSize;
-  canvas.height = memeSize;
+// Set the text style to that to which we are accustomed
 
-  //  Grab the nodes
-  var img = document.getElementById('start-image');
-  var topText = document.getElementById('top-text');
-  var bottomText = document.getElementById('bottom-text');
 
-  // When the image has loaded...
-  img.onload = function() {
+
+canvas.width = memeSize;
+canvas.height = memeSize;
+
+//  Grab the nodes
+var img = document.getElementById('start-image');
+var topText = document.getElementById('top-text');
+var bottomText = document.getElementById('bottom-text');
+
+// When the image has loaded...
+img.onload = function () {
     drawMeme()
-  }  
+}
 
-  topText.addEventListener('keydown', drawMeme)
-  topText.addEventListener('keyup', drawMeme)
-  topText.addEventListener('change', drawMeme)
+topText.addEventListener('keydown', drawMeme)
+topText.addEventListener('keyup', drawMeme)
+topText.addEventListener('change', drawMeme)
 
-  bottomText.addEventListener('keydown', drawMeme)
-  bottomText.addEventListener('keyup', drawMeme)
-  bottomText.addEventListener('change', drawMeme)
+bottomText.addEventListener('keydown', drawMeme)
+bottomText.addEventListener('keyup', drawMeme)
+bottomText.addEventListener('change', drawMeme)
 
-  function drawMeme() {
+function drawMeme() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.drawImage(img, 0, 0, memeSize, memeSize);
 
-    ctx.lineWidth  = 4;
+    ctx.lineWidth = 4;
     ctx.font = '20pt sans-serif';
     ctx.strokeStyle = 'black';
     ctx.fillStyle = 'white';
@@ -56,13 +57,13 @@
 
     wrapText(ctx, text2, x, y, 300, 28, true);
 
-  }
+}
 
-  function wrapText(context, text, x, y, maxWidth, lineHeight, fromBottom) {
+function wrapText(context, text, x, y, maxWidth, lineHeight, fromBottom) {
 
-    var pushMethod = (fromBottom)?'unshift':'push';
-    
-    lineHeight = (fromBottom)?-lineHeight:lineHeight;
+    var pushMethod = (fromBottom) ? 'unshift' : 'push';
+
+    lineHeight = (fromBottom) ? -lineHeight : lineHeight;
 
     var lines = [];
     var y = y;
@@ -70,23 +71,32 @@
     var words = text.split(' ');
 
     for (var n = 0; n < words.length; n++) {
-      var testLine = line + ' ' + words[n];
-      var metrics = context.measureText(testLine);
-      var testWidth = metrics.width;
+        var testLine = line + ' ' + words[n];
+        var metrics = context.measureText(testLine);
+        var testWidth = metrics.width;
 
-      if (testWidth > maxWidth) {
-        lines[pushMethod](line);
-        line = words[n] + ' ';
-      } else {
-        line = testLine;
-      }
+        if (testWidth > maxWidth) {
+            lines[pushMethod](line);
+            line = words[n] + ' ';
+        } else {
+            line = testLine;
+        }
     }
     lines[pushMethod](line);
 
     for (var k in lines) {
-      context.strokeText(lines[k], x, y + lineHeight * k);
-      context.fillText(lines[k], x, y + lineHeight * k);
+        context.strokeText(lines[k], x, y + lineHeight * k);
+        context.fillText(lines[k], x, y + lineHeight * k);
     }
 
 
-  }
+}
+
+function downloadCanvas(link, canvasId, filename) {
+    link.href = document.getElementById(canvasId).toDataURL();
+    link.download = filename;
+}
+
+document.getElementById('download').addEventListener('click', function () {
+    downloadCanvas(this, 'memecanvas', 'meme.png');
+}, false);
